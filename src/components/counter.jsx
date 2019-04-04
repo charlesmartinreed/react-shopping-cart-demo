b779c7277b448e57daa3461536adbd370894cf48
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-04 17:43:03 -0500

    Demonstrate basics of passing arguments to event handlers via arrow functions.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,23 +6,20 @@ export default class Counter extends Component {
     count: 0
   };
 
-  //in react, we don't modify the state directly
-  // not this.state.count++; this technically works, but react is not aware of it
-  // instead we use the setState method which brings the virtual DOM in line with the DOM
-  // so when we pass the object to setState it either finds and updates the value or creates one accordingly
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({
       count: this.state.count + 1
     });
   };
 
-  // note that, unlike in vanilla JS, we don't call the function attached to the onClick listener immediately
+  // need to pass an argument to an event handler? Use an arrow function
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
