773fcfa9f81ddd1213e9d308de7e6eded8206ce1
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-04 16:50:50 -0500

    Demonstrate render a list of items using .map.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,22 @@ import React, { Component } from "react";
 export default class Counter extends Component {
   // state obj comprises any data the Component might need
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
-
-  // one way to apply styles, passed as an object to the style attr in a component
-  // styles = {
-  //   fontSize: 50,
-  //   fontWeight: "bold"
-  // };
 
   render() {
     // why parenthesis? Because JS actually automatically places a ; at the end of a line if the end of the line is detected, which would preclude our return statement block from being executed.
     return (
+      // remember that each element in a loop needs a key in order to track it as the state/render changes
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
