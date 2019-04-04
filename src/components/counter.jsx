a840761343f950ec9fef14397c8a37c14ad76ef5
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-04 17:01:07 -0500

    Demonstrate basic example of conditional logic programming with helper render method and use of logical AND.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,34 +4,34 @@ export default class Counter extends Component {
   // state obj comprises any data the Component might need
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
-  render() {
-    // why parenthesis? Because JS actually automatically places a ; at the end of a line if the end of the line is detected, which would preclude our return statement block from being executed.
+  // since we can't use conditionals inside of our render method, we use this helper method to determine what to render based upon what
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are currently no tags</p>;
+
     return (
-      // remember that each element in a loop needs a key in order to track it as the state/render changes
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
-  getBadgeClasses() {
-    // depending on count, we change the class
-    let classes = "badge m-2 badge-";
-    return (classes += this.state.count === 0 ? "warning" : "primary");
+  // alternatively, we can use the logical && to handle conditionals
+  // basically, since we can apply the logical AND between non-boolean
+  // an empty string is falsey, non-empty is truthy
+  // true && 'Hi' && 1 - truthy, truthy, truthy, result is derived from last operand
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
